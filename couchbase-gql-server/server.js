4a510a7d948b34a262235cb1b4670e7fe5d34466
Eric Bishard <e.bishard@gmail.com>
2021-12-07 20:21:54 -0800

    Update pkgs to best version Update NodeJS SDK related code (to v3.2.3)

--- couchbase-gql-server/server.js
+++ couchbase-gql-server/server.js
@@ -4,162 +4,176 @@ const express = require('express')
 const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
-  
+
 const couchbase = require('couchbase')
-  
+
 const app = express()
-app.use(cors())
-const cluster = new couchbase.Cluster('couchbase://localhost', { 
-  username: cbUser, password: cbPass 
-})
-const bucket = cluster.bucket('travel-sample')
-var collection = bucket.defaultCollection();
 
-const schema = buildSchema(`
-  type Airline {
-    id: Int,
-    callsign: String,
-    country: String,
-    iata: String,
-    icao: String,
-    name: String,
-    type: String
-  }
-  input AirlineInput {
-    callsign: String,
-    country: String,
-    iata: String,
-    icao: String,
-    name: String,
-    type: String
-  }
-  type Query {
-    airlinesUK: [Airline],
-    airlineByKey(id: Int!): Airline
-    airlinesByRegion(region: String!): [Airline]
-  }
-  type Mutation {
-    updateAirline(id: Int!, input: AirlineInput): Airline
-  }
-`)
+const run = async () => {
 
-// Define N1QL Queries
-const airlinesUkQuery = `
-  SELECT airline.* 
-  FROM \`travel-sample\` AS airline 
-  WHERE airline.type = 'airline'
-  AND airline.country = 'United Kingdom'
-`
-const airlinesByRegionQuery = `
-  SELECT airline.* 
-  FROM \`travel-sample\` AS airline 
-  WHERE airline.type = 'airline'
-    AND airline.country = $REGION
-`
+  try {
 
-// Define Resolvers
-const root = {
-  airlinesUK: async () => {
-    const result = await cluster.query(airlinesUkQuery)
-    return result.rows
-  },
-  /*
-    query getAirlinesUK {
-      airlinesUK {
-        id
-        name
-        callsign
-        country
-        iata
-        icao
+    const cluster = await couchbase.connect(
+      'couchbase://localhost', {
+      username: cbUser, password: cbPass
+    })
+    const bucket = cluster.bucket('travel-sample')
+    const collection = bucket.defaultCollection()
+
+    const schema = buildSchema(`
+      type Airline {
+        id: Int,
+        callsign: String,
+        country: String,
+        iata: String,
+        icao: String,
+        name: String,
+        type: String
       }
-    }
-  */
-  airlinesByRegion: async ({region}) => {
-    const options = { parameters: { REGION: region } }
-    const result = await cluster.query(airlinesByRegionQuery, options)
-    return result.rows
-  },
-  /*
-    query getAirlinesByRegion($region: String!) {
-      airlinesByRegion(region:$region) {
-        id
-        name
-        callsign
-        country
-        iata
-        icao
+      input AirlineInput {
+        callsign: String,
+        country: String,
+        iata: String,
+        icao: String,
+        name: String,
+        type: String
       }
-    }
-    {
-      "region": "{{country}}"
-    }
-  */
-  airlineByKey: async ({id}) => {
-    const result = await collection.get(`airline_${id}`)
-    return result.value
-  },
-  /*
-    query getAirlineByKey($id: Int!) {
-      airlineByKey(id:$id) {
-        id
-        name
-        callsign
-        country
-        iata
-        icao
+      type Query {
+        airlinesUK: [Airline],
+        airlineByKey(id: Int!): Airline
+        airlinesByRegion(region: String!): [Airline]
       }
+      type Mutation {
+        updateAirline(id: Int!, input: AirlineInput): Airline
+      }
+    `)
+
+    // Define N1QL Queries
+    const airlinesUkQuery = `
+      SELECT airline.* 
+      FROM \`travel-sample\` AS airline 
+      WHERE airline.type = 'airline'
+      AND airline.country = 'United Kingdom'
+    `
+    const airlinesByRegionQuery = `
+      SELECT airline.* 
+      FROM \`travel-sample\` AS airline 
+      WHERE airline.type = 'airline'
+        AND airline.country = $REGION
+    `
+
+    // Define Resolvers
+    const root = {
+      airlinesUK: async () => {
+        const result = await cluster.query(airlinesUkQuery)
+        return result.rows
+      },
+      /*
+        query getAirlinesUK {
+          airlinesUK {
+            id
+            name
+            callsign
+            country
+            iata
+            icao
+          }
+        }
+      */
+      airlinesByRegion: async ({ region }) => {
+        const options = { parameters: { REGION: region } }
+        const result = await cluster.query(airlinesByRegionQuery, options)
+        return result.rows
+      },
+      /*
+        query getAirlinesByRegion($region: String!) {
+          airlinesByRegion(region:$region) {
+            id
+            name
+            callsign
+            country
+            iata
+            icao
+          }
+        }
+        {
+          "region": "{{country}}"
+        }
+      */
+      airlineByKey: async ({ id }) => {
+        const result = await collection.get(`airline_${id}`)
+        return result.content
+      },
+      /*
+        query getAirlineByKey($id: Int!) {
+          airlineByKey(id:$id) {
+            id
+            name
+            callsign
+            country
+            iata
+            icao
+          }
+        }
+        {
+          "id": {{id}}
+        }
+      */
+      updateAirline: async ({ id, input }) => {
+        const result = await collection.get(`airline_${id}`)
+
+        const newDocument = {
+          ...result.content,
+          callsign: input.callsign ? input.callsign : result.value.callsign,
+          country: input.country ? input.country : result.value.country,
+          iata: input.iata ? input.iata : result.value.iata,
+          icao: input.icao ? input.icao : result.value.icao,
+          name: input.name ? input.name : result.value.name,
+        };
+
+        console.log(newDocument)
+
+        await collection.upsert(`airline_${id}`, newDocument)
+        return newDocument
+      }
+      /*
+        mutation updateExistingAirline($id:Int!, $input:AirlineInput) {
+          updateAirline(id:$id, input:$input){
+            callsign
+            country
+            iata
+          }
+        }
+        {
+          "id": 112,
+          "input": {
+            "callsign": "FLYSTAR",
+            "country": "United Kingdom"
+          }
+        }
+      */
     }
-    {
-      "id": {{id}}
-    }
-  */
-  updateAirline: async ({id, input}) => {
-    const result = await collection.get(`airline_${id}`)
 
-    const newDocument = {
-      ...result.content, 
-      callsign: input.callsign ? input.callsign : result.value.callsign,
-      country: input.country ? input.country : result.value.country,
-      iata: input.iata ? input.iata : result.value.iata,
-      icao: input.icao ? input.icao : result.value.icao,
-      name: input.name ? input.name : result.value.name,
-    };
+    app.use(cors())
 
-    console.log(newDocument)
+    // the graphqlHTTP function accepts a schema, rootValue and graphiql 
+    // among other options for configuring our GraphQL Server
+    const serverPort = 4000
+    const serverUrl = '/graphql'
+    app.use(serverUrl, graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true
+    }))
 
-    await collection.upsert(`airline_${id}`, newDocument)
-    return newDocument
+    app.listen(
+      serverPort,
+      () => console.log(`GraphQL server running: http://localhost:${serverPort}${serverUrl}`)
+    )
+
+  } catch (e) {
+    console.log(e)
   }
-  /*
-    mutation updateExistingAirline($id:Int!, $input:AirlineInput) {
-      updateAirline(id:$id, input:$input){
-        callsign
-        country
-        iata
-      }
-    }
-    {
-      "id": 112,
-      "input": {
-        "callsign": "FLYSTAR",
-        "country": "United Kingdom"
-      }
-    }
-  */
 }
 
-// the graphqlHTTP function accepts a schema, rootValue and graphiql 
-// among other options for configuring our GraphQL Server
-const serverPort = 4000
-const serverUrl = '/graphql'
-app.use(serverUrl, graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}))
-
-app.listen(
-  serverPort, 
-  () => console.log(`GraphQL server running: http://localhost:${serverPort}${serverUrl}`)
-)
+run()
